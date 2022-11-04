cb1f5e0981aaf1fe3255305115704388d025ecd0
JeielSouza <contatojeielsouza@gmail.com>
2022-11-04 09:12:22 -0300

    v1 Organizacao 11_4_2022

---
+++ C/main.c
@@ -0,0 +1,19 @@
+// apenas um teste de inicializacao para commitar
+
+#include <stdio.h>
+
+int main()
+{
+
+    int Inter = 313122;
+    char Name[] = {"Jeiel Teste"};
+    float flut = 321.5;
+
+    printf("Apenas um Teste\n");
+    printf("Apenas um teste!!!");
+    printf("%f",flut);
+    printf("%d", Inter);
+    printf("%s", Name);
+    
+    return 0;
+}
---
+++ Python/teste.py
@@ -0,0 +1,2 @@
+print("Teste")
+jao = 31
