7387889ca16e3c30db24c4f46ccec3c3b1b1d734
JeielSouza <contatojeielsouza@gmail.com>
2023-02-06 22:18:38 -0300

    Processamento_de_dados_Java

--- JAVA/One.java
+++ JAVA/One.java
@@ -1,34 +1,12 @@
-public class One
+public class One 
 {
     public static void main(String[] args)
     {
-        String produto = "Computador";
-        String produto2 = "Mesa Escritorio";
-        int age = 30;
-        int cod = 5290; 
-        char sex = 'F'; 
+         int x = 0; 
+         double y = 4.5;
 
-        double size = 53.234567;
-        double price = 2100.00;
-        double price2 = 650.50;
-        
-        System.out.printf("PRODUTOS: %n");
-        System.out.printf("1 - %s %n PRECO $ %.2f %n", produto, price);
-        System.out.printf("2 - %s %n PRECO $ %.2f %n", produto2, price2);
-        
-        
-        System.out.printf("%n");
+         x = (int)y;  
 
-        System.out.println("CLIENTE: ");
-        System.out.println("IDADE: " + age);
-        System.out.println("COD: " + cod);
-        System.out.println("GENERO: " + sex);
-
-        System.out.printf("%n");
-
-        System.out.printf("TAMANHO DA MESA COM OITO DECIMAIS: %.8f %n COM TRES DECIMAIS: %.3f", size, size);
-
-
-
+         System.out.println(x);
     }
 }
